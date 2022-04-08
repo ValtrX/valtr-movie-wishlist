dcea4b0c077b17f0799d7f355fda6bab1f3e1beb
ValtrX <grateroldiaz@hotmail.com>
2022-04-07 21:53:36 -0400

    adding debouncer w/ David

--- index.js
+++ index.js
@@ -1,3 +1,49 @@
-fetch('http://www.omdbapi.com/?s=spiderman&apikey=df700428')
+const searchBar = document.querySelector('#search-bar--input')
+const moveItemContainer = document.querySelector('.movie-grid--item')
+
+const apiFetch = (searchBarTyped) => fetch('http://www.omdbapi.com/?s=' + searchBarTyped + '&apikey=df700428')
   .then(response => response.json())
-  .then(data => console.log(data));
+  .then(data => {
+    let html = ""
+    if (data.Response === 'False') {
+      return
+    }
+    data.Search.forEach(element => {
+      html += `
+        <div class="item--content">
+        <div class="item-title">
+        
+        <h4>${element.Title}</h4>
+        
+        </div>
+        </div>
+        `
+    });
+    moveItemContainer.innerHTML = html
+  });
+
+let requestsInterval 
+
+const promiseOptions = (inputValue) =>
+  new Promise((resolve) => {
+    requestsInterval = setTimeout(() => {
+      resolve(apiFetch(inputValue));
+    }, 500);
+  });
+
+searchBar.addEventListener("keyup", () => {
+  
+  clearTimeout(requestsInterva)
+
+  const searchBarTyped = searchBar.value
+
+  if (searchBarTyped.length === 0) {
+    moveItemContainer.innerHTML = ""
+    return
+  }
+  promiseOptions(searchBarTyped)
+
+});
+
+
+
