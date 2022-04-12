9fca92b7a5aa8ee11df92c3d49dfc7c90e32af7e
ValtrX <grateroldiaz@hotmail.com>
2022-04-12 15:38:58 -0400

    adding css to the project

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 const searchBar = document.querySelector('#search-bar--input')
-const moveItemContainer = document.querySelector('.movie-grid--item')
+const moveItemContainer = document.querySelector('.movie-grid')
 
 const apiFetch = (searchBarTyped) => fetch('http://www.omdbapi.com/?s=' + searchBarTyped + '&apikey=df700428')
   .then(response => response.json())
@@ -8,15 +8,35 @@ const apiFetch = (searchBarTyped) => fetch('http://www.omdbapi.com/?s=' + search
     if (data.Response === 'False') {
       return
     }
+    console.log(data);
     data.Search.forEach(element => {
       html += `
-        <div class="item--content">
-        <div class="item-title">
-        
-        <h4>${element.Title}</h4>
-        
-        </div>
-        </div>
+
+        <div class="movie-grid--item">
+                
+                <div class="item--img">
+                    <img src="${element.Poster}">
+                </div>
+
+                <div class="item--content">
+
+                    <div class="item-title">
+                        <h4>${element.Title}</h4>
+                        <p class="item--score">⭐6.1</p>
+                    </div>
+                    <div class="item--data">
+                        <p>164 min</p>
+                        <p>Action, Drama, Sci-fi</p>
+                        <a href="#"><img src="img/plus-icon.svg"> Watchlist</a>
+                    </div>
+                    <div class="item--desc">
+                        <p>A blade runner must pursue and terminate four replicants who stole a ship in space, and have returned to Earth to find their creator. A blade runner must pursue and terminate four replicants who stole a ship in space, and have returned to Earth to find their creator.A blade runner must pursue and terminate four replicants who stole a ship in space, and have returned to Earth to find their creator.A blade runner must pursue and terminate four replicants who stole a ship in space, and have returned to Earth to find their creator.</p>
+                    </div>
+
+                </div> 
+
+            
+            </div>
         `
     });
     moveItemContainer.innerHTML = html
@@ -33,7 +53,7 @@ const promiseOptions = (inputValue) =>
 
 searchBar.addEventListener("keyup", () => {
   
-  clearTimeout(requestsInterva)
+  clearTimeout(requestsInterval)
 
   const searchBarTyped = searchBar.value
 
